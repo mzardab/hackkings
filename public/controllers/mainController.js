7f43322fe4b7f20640779a459ecef5b2e17c0d39
Luke Exton <lukeexton@sky.com>
2016-11-26 16:15:07 +0000

    <LE> added templates for CSS and Angular

---
+++ public/controllers/mainController.js
@@ -0,0 +1,12 @@
+var app = angular.module('hackkings', []);
+
+app.controller('MainController', function($scope, $http) {
+
+    // insert code here
+
+    // Function example (keeping data within scope)
+    // $scope.getData = function(something){
+        
+    // };
+
+});
